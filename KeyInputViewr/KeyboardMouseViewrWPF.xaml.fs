7681e5a52ed6785c3cf3af25d02657f7c16a4829
regerege <regerege6@gmail.com>
2012-05-13 11:34:57 +0900

    Add Mouse Event

--- KeyInputViewr/KeyboardMouseViewrWPF.xaml.fs
+++ KeyInputViewr/KeyboardMouseViewrWPF.xaml.fs
@@ -5,7 +5,7 @@ open System
 open System.Windows
 open System.Windows.Controls
 
-type KeyboardMouseViewrWPF() as w =
+type KeyboardMouseViewrWPF() =
     inherit Window()
 
     let _uri = new Uri("KeyboardMouseViewrWPF.xaml", UriKind.Relative)
@@ -15,21 +15,41 @@ type KeyboardMouseViewrWPF() as w =
     let _defaultStyle = getStyle "_BorderStyle"
     let _pushedStyle = getStyle "_BorderStylePushed"
 
-//#region キーマッピング関連
-    let getKeyName key = sprintf "b_%A" <| enum<Keys> key
-    let getControl key = _base.FindName(getKeyName key) :?> Border
-    let seqKeys =
+    /// 汎用的なコントロール取得シーケンス
+    let createSeq n f =
         Seq.cache <|
-            Seq.init 0xFF (((+)1) >> (fun x -> (x, getControl x)))
+            Seq.init n (((+)1) >> (fun x -> (x, f x)))
             |> Seq.choose (fun t ->
                 if (snd t) <> null then Some t
                 else None
             )
+    /// 汎用的にコントロールを取得する。
+    let getCtrl nm f key = sprintf nm <| f key |> _base.FindName
+//#region キーマッピング関連
+    let seqKeys =
+        createSeq 0xFF <| (fun (key:int32) -> getCtrl "b_%A" enum<Keys> key :?> Border)
+//#endregion
+//#region マウスマッピング関連
+    let seqMouseButtons =
+        createSeq 0x1E <| (fun (key:int32) -> getCtrl "img_b_%A" enum<MouseButtons> key :?> Image)
+    let seqMouseWheels =
+        createSeq 0x0E <| (fun (key:int32) -> getCtrl "img_h_%A" enum<MouseWheel> key :?> Image)
 //#endregion
 
     do
         _base.MouseLeftButtonDown.Add(fun _ -> _base.DragMove())
 
     member w.Base = _base
+    /// Keyboardのキーマッピング
     member w.Keys = seqKeys
+    /// Mouseのキーマッピング
+    member w.MouseButtons = seqMouseButtons
+    /// MouseWheelマッピング
+    member w.MouseWheel = seqMouseWheels
+    /// MouseWheelのボタン
+    member w.MouseWhellButton = _base.FindName("img_b_Middle") :?> Image
+
+    /// コントロールのON/OFF判定時のStyleを取得する。
     member w.GetButtonStyle b = if b then _pushedStyle else _defaultStyle
+    /// イメージのON/OFF判定時のVisibilityを取得する。
+    member w.GetImageVisible b = if b then Visibility.Visible else Visibility.Hidden
--- KeyInputViewr/Program.fs
+++ KeyInputViewr/Program.fs
@@ -5,6 +5,7 @@ open KeyInputViewr
 open KeyInputViewr.Core
 open System
 open System.Windows
+open System.Windows.Controls
 
 let app = new Application()
 let wpf = new KeyboardMouseViewrWPF()
@@ -17,18 +18,21 @@ GlobalHook.Filter <- None
 
 // イベントの追加
 GlobalHook.KeyMouseEvent.Add(fun x ->
-//    printfn "Key: %A" x.Key
-//    printfn "ScanCode: %A" x.ScanCode
-////    printfn "KeyBit: %A" x.KeyBit
-////    printfn "Mouse: %A" x.Mouse
-////    printfn "Wheel: %A" x.Wheel
-//    printfn ""
-
     // すべてのキーマップと押されたキーのマップをチェックして、
     // 表示 / 非表示を切り替える。
     wpf.Keys
     |> Seq.iter(fun (n,c) ->
         c.Style <- wpf.GetButtonStyle x.KeyBit.[n])
+    wpf.MouseButtons
+    |> Seq.iter(fun (n,c) ->
+        let b = x.Mouse &&& enum<MouseButtons> n <> MouseButtons.None
+        c.Visibility <- wpf.GetImageVisible b)
+    if x.Wheel <> MouseWheel.None then
+        wpf.MouseWhellButton.Visibility <- wpf.GetImageVisible true
+    wpf.MouseWheel
+    |> Seq.iter(fun (n,c) ->
+        let b = x.Wheel &&& enum<MouseWheel> n <> MouseWheel.None
+        c.Visibility <- wpf.GetImageVisible b)
 )
 
 #if COMPILED
